895b7e25b4ad211df0d97e608b52409949a3b3ae
Patrick Werner <kug3lblitz@protonmail.com>
2018-05-15 12:08:40 -0400

    added daily low temps to graph, temps for year

--- high_low.py
+++ high_low.py
@@ -2,8 +2,8 @@ import csv
 from matplotlib import pyplot as plt
 from datetime import datetime
 
-# get dates and high temps from file, convert from strs to ints to visualize
-filename = 'sitka_weather_07-2014.csv'
+# get dates, low and high temps from file, convert strs to ints to visualize
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -12,7 +12,7 @@ with open(filename) as f:
     # for index, column_header in enumerate(header_row):
         # print(index, column_header)
 
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
@@ -20,16 +20,20 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
 
+        low = int(row[3])
+        lows.append(low)
+
     # plot data
     fig = plt.figure(dpi=128, figsize=(10,6))
     plt.plot(dates, highs, c='red')
+    plt.plot(dates, lows, c='blue')
 
     # format plot
-    plt.title("Daily high temperatures, July 2014, Sitka, AK", fontsize=24)
-    plt.xlabel('', fontsize=16)
+    plt.title("Daily high and low temperatures, 2014, Sitka, AK", fontsize=24)
+    plt.xlabel('', fontsize=12)
     # draw xlabels diagonally to prevent overlap
     fig.autofmt_xdate()
     plt.ylabel("Temperature (F)", fontsize=16)
-    plt.tick_params(axis='both', which='major', labelsize=16)
+    plt.tick_params(axis='both', which='major', labelsize=12)
 
     plt.show()
